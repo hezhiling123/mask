78dc0e2eca7434d3894136ddfd7b314f7eb7ef4d
hezhiling123 <1605489109@qq.com>
2021-03-02 22:28:30 +0800

    配置完成微服务启动

--- mask-user-service/src/main/java/cn/hezhiling/users/service/impl/TestServiceImpl.java
+++ mask-user-service/src/main/java/cn/hezhiling/users/service/impl/TestServiceImpl.java
@@ -2,7 +2,9 @@ package cn.hezhiling.users.service.impl;
 
 import cn.hezhiling.sys.service.TestService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -15,9 +17,10 @@ import java.util.Map;
  */
 @Slf4j
 @RestController
-@RequestMapping("/user/sys/service/TestService")
+@RequestMapping("/test")
 public class TestServiceImpl implements TestService {
 	@Override
+	@RequestMapping(value = "/loginByToken", method = RequestMethod.GET)
 	public Map<String, Object> test(String userId) {
 		Map<String, Object> result = new HashMap<>(1);
 		result.put("userId", userId);
