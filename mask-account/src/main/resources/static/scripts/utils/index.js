4d5c696412de40dbf9baa942574a55aaadb61249
hezhiling <1605489109@qq.com>
2022-03-23 19:31:41 +0800

    代码格式化1

--- mask-account/src/main/resources/static/scripts/utils/index.js
+++
@@ -1,8 +0,0 @@
-define(['utils/uLodash', 'utils/uMessageBox'], function (uLodash, messageBox) {
-    return {
-        install(Clazz) {
-            Clazz.use(uLodash)
-            Clazz.use(messageBox)
-        }
-    }
-})
